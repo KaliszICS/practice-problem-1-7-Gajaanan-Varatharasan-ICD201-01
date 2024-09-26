d6b31b5f115f00cccc8a04b7966b231a0eefff34
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-09-26 00:29:55 +0000

    f

--- PP1_7.py
+++ PP1_7.py
@@ -1,29 +1,29 @@
 
 
-#q1()
+def q1():
   #Write code here
-print(bool("Hello"))
-#q2()
+  print(bool("Hello"))
+def q2():
   #Write code here
-number=eval(input("Enter number"))
-bool1=5<number
-print(bool1)
-#q3()
+  number=eval(input("Enter number"))
+  bool=5<number
+  print(bool)
+def q3():
   #Write code here
-value=input("Enter a")
-bool1='a'==value
-print(bool1)
-#q4()
+  value=input("Enter a")
+  bool='a'==value
+  print(bool)
+def q4():
   #Write code here
-word=input("Enter a word")
-bool=(word<"google")
-print(bool)
-#q5()
+  word=input("Enter a word")
+  bool=(word<"google")
+  print(bool)
+def q5():
   #Write code here
-number1=eval(input("Enter a number"))
-number2=eval(input("Enter another number"))
-bool1=40<(number1*number2)
-print(f"Your numbers multiplied together are greater than 40: {bool1}")
+  number1=eval(input("Enter a number"))
+  number2=eval(input("Enter another number"))
+  bool1=40<(number1*number2)
+  print(f"Your numbers multiplied together are greater than 40: {bool1}")
 #Do edit the code below
 #Comment the lines below when running your tests
 
