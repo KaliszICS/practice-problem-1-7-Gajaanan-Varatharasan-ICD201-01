12fbfae68db1894b63bdd8aa73ab451f28d05299
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-09-26 00:12:36 +0000

    j

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,34 @@
 
 
-q1():
+#q1()
   #Write code here
-
-q2():
+print(bool("Hello"))
+#q2()
   #Write code here
-
-q3():
+number=eval(input("Enter number"))
+bool1=5<number
+print(bool1)
+#q3()
   #Write code here
-
-q4():
+value=input("Enter a")
+bool1='a'==value
+print(bool1)
+#q4()
   #Write code here
-
-q5():
+word=input("Enter a word")
+bool=(word<"google")
+print(bool)
+#q5()
   #Write code here
-
+number1=eval(input("Enter a number"))
+number2=eval(input("Enter another number"))
+bool1=40<(number1*number2)
+print(f"Your numbers multiplied together are greater than 40: {bool1}")
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
